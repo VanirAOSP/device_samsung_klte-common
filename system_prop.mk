742c322c2d05865286657a3c658735ee3c2b0bb5
nuclearmistake <nuclearmistake@gmail.com>
2014-08-31 09:05:43 -0400

    klte: turn off sensor debugging
    Change-Id: I22e950f330537ee51a90e0d4b5561775ccb3f775

--- system_prop.mk
+++ system_prop.mk
@@ -27,7 +27,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.lte_vrat_report=1
-
-# Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sensors=1
