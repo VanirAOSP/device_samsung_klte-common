11c1cada54d3832739639485bf817f0c33b0bbfb
nuclearmistake <nuclearmistake@gmail.com>
2014-09-30 10:25:28 -0400

    klte-common: try the new Widevine+Stagefright compat flag to fix DRM maybe?
    Change-Id: I7bc7f3d50e8e04c7b86b278dc125a4dc9a9c3af1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,5 +91,7 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 
+COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
+
 # inherit from the proprietary version
 -include vendor/samsung/klte-common/BoardConfigVendor.mk
